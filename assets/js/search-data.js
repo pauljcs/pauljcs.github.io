8ac07b9aca79b87981e029179874fe4404c68a66
pauljcs <39455913+pauljcs@users.noreply.github.com>
2025-02-25 14:08:58 +0000

    Deploying to gh-pages from @ pauljcs/pauljcs.github.io@4159bac6b74516a5555f75cfcde89cb3dc17918f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -520,7 +520,7 @@ ninja.data = [{
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("", "_blank");
         },
       },{
       id: 'light-theme',
